94ce80f2caa2bf1128d840be99e3a521a39b9c5e
Kohei Suzuki <jingle@sfc.wide.ad.jp>
2018-02-08 19:14:00 +0900

    initial commit

---
+++ repo.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+var currentID int
+var todos Todos
+
+// Give us some seed data
+func init() {
+	RepoCreateTodo(Todo{Name: "Write presentation"})
+	RepoCreateTodo(Todo{Name: "Host meetup"})
+}
+
+func RepoFindTodo(id int) Todo {
+	for _, t := range todos {
+		if t.ID == id {
+			return t
+		}
+	}
+	return Todo{}
+}
+
+func RepoCreateTodo(t Todo) Todo {
+	currentID += 1
+	t.ID = currentID
+	todos = append(todos, t)
+	return t
+}
+
+func RepoDestroyTodo(id int) error {
+	for i, t := range todos {
+		if t.ID == id {
+			todos = append(todos[:i], todos[i+1:]...)
+			return nil
+		}
+	}
+	return fmt.Errorf("Could not find Todo with id of %d to delete", id)
+}
